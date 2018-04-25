78e0f5bc57be458634e9331904cb2be3cd003008
mattmaltieri <38731536+mattmaltieri@users.noreply.github.com>
2018-04-25 15:40:48 -0400

    Updated generate_access_token.py
    Made some changes to accomodate what appears to be a new format for the oauth URL, removed some needless comments and debug-type lines that were print() -ing data that was simply cluttering the script and output ... and ensured it works again!

--- generate_access_token.py
+++ generate_access_token.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Lifted from http://bit.ly/1qpxJlj
+# Originally Lifted from http://bit.ly/1qpxJlj back in 2016.
 
 from oauthlib.oauth1 import SIGNATURE_RSA
 from requests_oauthlib import OAuth1Session
@@ -14,77 +14,47 @@ def read(file_path):
 
 # The Consumer Key created while setting up the "Incoming Authentication" in
 # JIRA for the Application Link.
-CONSUMER_KEY = u''
+CONSUMER_KEY = u'0dab23cd64d14529894a470da541306f'
 
 # The contents of the rsa.pem file generated (the private RSA key)
-RSA_KEY = read('/PATH/TO/YOUR/RSA_CERT.pem')
-#print("This is the key I read:\n")
-#print(RSA_KEY)
-#print("\n")
-
+RSA_KEY = read('/opt/stackstorm/packs/jira/jira.pem')
 
 # The URLs for the JIRA instance
-JIRA_SERVER = 'http://YOUR-JIRA-URL:PORT'
+JIRA_SERVER = 'http://jira-7-dev:8080'
 REQUEST_TOKEN_URL = JIRA_SERVER + '/plugins/servlet/oauth/request-token'
 AUTHORIZE_URL = JIRA_SERVER + '/plugins/servlet/oauth/authorize'
 ACCESS_TOKEN_URL = JIRA_SERVER + '/plugins/servlet/oauth/access-token'
-print("Jira Server: " + JIRA_SERVER + "\n")
-print("AccessTokenURL: " + ACCESS_TOKEN_URL + "\n\n")
+
+#Print what we think your JIRA server should be (What you gave us above):
+print("\nJIRA SERVER: " + JIRA_SERVER + "")
+print("CONSUMER_KEY: " + CONSUMER_KEY + "")
 
 # Step 1: Get a request token
-
 oauth = OAuth1Session(CONSUMER_KEY, signature_type='auth_header',
                       signature_method=SIGNATURE_RSA, rsa_key=RSA_KEY)
 request_token = oauth.fetch_request_token(REQUEST_TOKEN_URL)
 
-
-
-print("\n\nSTEP 1: GET REQUEST TOKENS:\n")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\nSTEP 1: GET THE REQUEST TOKENS:")
 tokens=request_token
-
-#print(tokens['oauth_token'])
 token=tokens['oauth_token']
-#print(tokens['oauth_token_secret'])
 secret=tokens['oauth_token_secret']
 print("Token: " + token)
-print("Secret: " + secret + "\n") 
+print("Secret: " + secret + "\n\n")
 
-
-
-#print("  oauth_token={}".format(request_token['oauth_token']))
-#print("  oauth_token_secret={}".format(request_token['oauth_token_secret']))
-print("\n")
-
-
-# Step 2: Get the end-user's authorization
-
-print("STEP2: AUTHORIZATION")
+# Step 2: Get the end-user to provide JIRA authorization
+print("STEP2: AUTHORIZE FROM JIRA")
 print("Visit to the following URL to provide authorization:")
-print(AUTHORIZE_URL + "?" + token)
-
-#print("  {}?oauth_token={}".format(AUTHORIZE_URL, request_token['oauth_token']))
-print("\n")
-
-while raw_input("Press any key to continue..."):
+print(AUTHORIZE_URL + "?oauth_token=" + token)
+print("\n^^Copy the above URL, visit it, and hit 'accept' on the prompt that follows.^^\n")
+while raw_input("Press any key to continue ONLY AFTER authorizing your token...!"):
     pass
 
 # XXX: This is an ugly hack to get around the verfication string
 # that the server needs to supply as part of authorization response.
 # But we hard code it.
 oauth._client.client.verifier = u'verified'
-# Step 3: Get the access token
-
 access_token = oauth.fetch_access_token(ACCESS_TOKEN_URL)
-
-print("Copy the following URL, and append the access token printed above -- visit it in a browser & accept:")
-print("http://10.42.100.117:8080/plugins/servlet/oauth/authorize?oauth_token=PASTETOKENHERE")
-
-print("STEP2: GET ACCESS TOKEN")
-
 print(access_token)
-#print("  oauth_token={}".format(access_token['oauth_token']))
-#print("  oauth_token_secret={}".format(access_token['oauth_token_secret']))
 print("\n")
 
-# Now you can use the access tokens with the JIRA client. Hooray!
-
+# You should now be able to use your access token with JIRA ... Hooray!
